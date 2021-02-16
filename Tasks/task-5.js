a04d0ed2a2d040317f82e5ffaf172aeb65bcbab6
Stefferson Thallys <steffersonthallys@gmail.com>
2021-02-16 20:06:24 -0300

    Soma dos pares
    Dada uma lista de inteiros e um único valor de soma, retorne os dois primeiros valores (analise da esquerda, por favor) na ordem de aparecimento que somam para formar a soma.

---
+++ Tasks/task-5.js
@@ -0,0 +1,7 @@
+function sumPairs(ints, s) {
+    var seen = {}
+    for (var i = 0; i < ints.length; ++i) {
+      if (seen[s - ints[i]]) return [s - ints[i], ints[i]]
+      seen[ints[i]] = true
+    }
+  }
